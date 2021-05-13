623bdf295d6dec3eddd6911deeaa0691ec0f577f
Ardra2517 <74819734+Ardra2517@users.noreply.github.com>
2021-05-13 19:11:34 +0530

    Add files via upload

--- Mango.js
+++ Mango.js
@@ -3,13 +3,11 @@ class mango{
 	{
 		var options={
 			isStatic:true,
-			restitution :0,
-            friction :1,
 			}
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.image=loadImage("images/mango.png")
+		this.image=loadImage("mango.png")
 		this.body=Bodies.circle(this.x, this.y, this.r, options)
 		World.add(world, this.body);
 	}
--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint=Matter.Constraint;
 
-var tree, stone,ground, launcher;
+var treeImage;
+var stone,ground,launcher;
 var mango1;
 var world,boy;
 
 function preload(){
-	boy=loadImage("images/boy.png");
+	boy=loadImage("boy.png");
+  treeImage=loadImage("tree.png");
   }
 
 function setup() {
@@ -18,13 +20,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
   
-  mango1=new mango(1100,100,30);
-  mango2=new mango(1200,200,30);
-  mango3=new mango(900,190,30);
-  mango4=new mango(1000,170,30);
-  mango5=new mango(1100,190,30);
+  mango1=new mango(800,300,30);
+  mango2=new mango(600,400,30);
+  mango3=new mango(900,400,30);
+  mango4=new mango(700,300,30);
+  mango5=new mango(750,400,30);
 
-	tree=new Tree(1050,580);
+	// tree=new Tree(1050,580);
   ground=new Ground(width/2,600,width,20);
   stone=new Stone(300,340,40);
 	launcher=new Launcher(stone.body,{x:340, y:430});
@@ -39,14 +41,10 @@ function draw() {
   Engine.update(engine);
 
   image(boy,300,360,200,300);
+  image(treeImage,500,200,500,500);
 
-  // detectCollision(stone,mango1);
-  // detectCollision(stone,mango2);
-  // detectCollision(stone,mango3);
-  // detectCollision(stone,mango4);
-  // detectCollision(stone,mango5);
 
-  tree.display();
+  // tree.display();
   mango1.display();
   mango2.display();
   mango3.display();
@@ -55,16 +53,6 @@ function draw() {
 
   ground.display();
   stone.display();
-}
-
-function detectCollision(lstone,lmango){
- mangoBodyPosition=lmando.body.position
- stoneBodyPosition=lstone.body.position
- 
- var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y,mangoBodyPosition.x, mangoBodyPosition.y)
- if(distance<=lmango.r+lstone.r){
-  Matter.Body.setStatic(lmango.body,false);
- }
 }
 
 function mouseDragged(){
